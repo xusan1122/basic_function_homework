a760e625ad633019cdaf75e760170da72ef6dea6
Mr.Xusan <60698829+xusan1122@users.noreply.github.com>
2023-02-23 09:48:56 +0500

    Update basic_func06.dart

--- bin/basic_func06.dart
+++ bin/basic_func06.dart
@@ -1,7 +1,7 @@
 // Create a function called func.
 // Create a function argument called ‘number’ of type int the two-digit numbers will be given.
 // Find the reverse of the number and return to a variable called ‘answer’ return answer
-int func(number) {
+int func(int number) {
   int a = number % 10;
   int b = number ~/ 10;
   int answer = a * 10 + b;
